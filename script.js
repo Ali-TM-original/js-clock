a966b93d39f77dc9c8866a5c626352f3ebf38b23
Ali-TM-original <76595455+Ali-TM-original@users.noreply.github.com>
2021-05-01 21:44:28 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function what_time(){
-    var time_label = document.getElementById("main_clock");
+    let time_label = document.getElementById("main_clock");
     let current_time = new Date().toLocaleTimeString();
-    time_label.innerHTML = new Date().toLocaleTimeString();
-    console.log(current_time);
+    time_label.innerHTML = current_time
+
 }
 
 setInterval(what_time, 1000);
